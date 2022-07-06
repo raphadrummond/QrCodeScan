3654bdd466e650125c8660c39099f65dd26b88cf
Raphael Venâncio Drummond <raphael.drummond@gdatabit.local>
2022-07-06 17:15:27 -0300

    App Scanner de QR Code

---
+++ app/src/main/java/com/raphaeldrummond/qrcodescan/CaptureAct.java
@@ -0,0 +1,7 @@
+package com.raphaeldrummond.qrcodescan;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity
+{
+}
---
+++ app/src/main/java/com/raphaeldrummond/qrcodescan/MainActivity.java
@@ -0,0 +1,58 @@
+package com.raphaeldrummond.qrcodescan;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnScan =findViewById(R.id.btnScan);
+        btnScan.setOnClickListener(v->
+        {
+            scanCode();
+
+        });
+    }
+
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Teste 123");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result ->{
+        if(result.getContents() !=null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
+
+
+
+
+}
